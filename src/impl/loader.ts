ec9a663ac8e3a49fa2c8cd123c360ec5f75a7db3
Omar Othman <omar.othman@live.com>
2022-10-19 13:16:17 +0300

    Implement package loader

--- src/impl/loader.ts
+++ src/impl/loader.ts
@@ -15,6 +15,40 @@ export default class DefaultProjectLoader implements ProjectLoader {
     if (!directories.length) {
       throw new EmptyProjectError();
     }
-    throw new NoPackagesError();
+    const packages = directories.filter((d) => d.isDirectory());
+    if (!packages.length) {
+      throw new NoPackagesError();
+    }
+    return Promise.all(
+      packages.map(async (p) => {
+        const modules = await this.loadModules(p.name);
+        return { packageName: p.name, modules };
+      }),
+    );
+  }
+
+  private async loadModules(packageName: string) {
+    const files = await this.getDeepFiles(packageName);
+    return Promise.all(
+      files.map(async (f) => {
+        const buffer = await fs.readFile(`${this.projectDirectory}/${f}`);
+        return buffer.toString();
+      }),
+    );
+  }
+
+  private async getDeepFiles(parentDir: string): Promise<string[]> {
+    const entries = await fs.readdir(`${this.projectDirectory}/${parentDir}`, {
+      withFileTypes: true,
+    });
+    const deepFiles = await Promise.all(
+      entries.map(async (e) => {
+        if (e.isDirectory()) {
+          return this.getDeepFiles(`${parentDir}/${e.name}`);
+        }
+        return [`${parentDir}/${e.name}`];
+      }),
+    );
+    return deepFiles.flat();
   }
 }
--- test/integration/impl/loader.test.ts
+++ test/integration/impl/loader.test.ts
@@ -13,34 +13,56 @@ import config from '../config';
 test.beforeEach(() => createTestDirectory());
 test.after.always(() => cleanupTestDirectory());
 
-test('should throw if project directory does not exist', async (t) => {
+test.serial('should throw if project directory does not exist', async (t) => {
   const loader = new LoaderProxy('nonExistingProject');
   await t.throwsAsync(loader.load(), { instanceOf: DirectoryNotFoundError });
 });
 
-test('should throw if project directory is empty', async (t) => {
+test.serial('should throw if project directory is empty', async (t) => {
   const projectName = 'emptyProject';
   const loader = new LoaderProxy(projectName);
   await createDirectoryRecursively(projectName);
   await t.throwsAsync(loader.load(), { instanceOf: EmptyProjectError });
 });
 
-test('should throw if project directory contains no folders', async (t) => {
+test.serial('should throw if project directory contains no folders', async (t) => {
   const projectName = 'testProject';
   const loader = new LoaderProxy(projectName);
   await createFile(`${projectName}/index.ts`);
   await t.throwsAsync(loader.load(), { instanceOf: NoPackagesError });
 });
 
+test.serial('should detect folders as packages but not files', async (t) => {
+  const projectName = 'testProject';
+  const loader = new LoaderProxy(projectName);
+  await createFile(`${projectName}/index.ts`);
+  await createFile(`${projectName}/package1/module1.ts`, "console.log('module1');");
+  await createFile(`${projectName}/package2/subdirectory/module2.ts`, "console.log('module2');");
+  await createFile(`${projectName}/package2/module3.ts`, "console.log('module3');");
+  const packages = await loader.load();
+  t.is(packages.length, 2);
+  t.is(packages.find((p) => p.packageName === 'package1')?.modules.length, 1);
+  t.true(
+    packages.find((p) => p.packageName === 'package1')?.modules.includes("console.log('module1');"),
+  );
+  t.is(packages.find((p) => p.packageName === 'package2')?.modules.length, 2);
+  t.true(
+    packages.find((p) => p.packageName === 'package2')?.modules.includes("console.log('module2');"),
+  );
+  t.true(
+    packages.find((p) => p.packageName === 'package2')?.modules.includes("console.log('module3');"),
+  );
+});
+
 class LoaderProxy {
   private loader = new DefaultProjectLoader(path.join(config.testDirectory, this.projectName));
   constructor(private projectName: string) {}
-  load(): Promise<Modules[]> {
+  load(): Promise<PackageModules[]> {
     return this.loader.load();
   }
 }
 
-interface Modules {
+interface PackageModules {
   packageName: string;
   modules: string[];
 }
