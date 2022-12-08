53f4ec0e4d663e58124c3ff4b9c0de6d7629709e
Omar Othman <omar.othman@live.com>
2022-12-08 19:53:16 +0300

    Load tsconfig from cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import path from 'path';
+import path from 'node:path';
+import ts from 'typescript';
 import DefaultProjectAnalyzer from './impl/analyzer';
 import MetricsFacade from './impl/facade';
 import DefaultProjectLoader from './impl/loader';
@@ -14,11 +15,29 @@ main().catch((err) => {
 async function main() {
   /* c8 ignore next */
   const projectDirectory = process.argv[2] ? path.resolve(process.argv[2]) : process.cwd();
-
   const loader = new DefaultProjectLoader(projectDirectory);
-  const analyzer = new DefaultProjectAnalyzer();
+
+  const { baseUrl, paths } = loadTsConfig(process.argv[3]);
+  const analyzer = new DefaultProjectAnalyzer(projectDirectory, baseUrl, paths);
+
   const facade = new MetricsFacade(loader, analyzer);
   const reporter = new DefaultReporter();
   const reports = await facade.analyze();
   reporter.report(reports);
+}
+
+function loadTsConfig(filename?: string) {
+  const configFilename = ts.findConfigFile('./', (fn) => ts.sys.fileExists(fn), filename);
+
+  /* c8 ignore next 3 */
+  if (!configFilename) {
+    return { baseUrl: undefined, paths: undefined };
+  }
+
+  const configFile = ts.readConfigFile(configFilename, (fp) => ts.sys.readFile(fp));
+  const compilerOptions = ts.parseJsonConfigFileContent(configFile.config, ts.sys, './');
+  return {
+    baseUrl: compilerOptions.options.baseUrl,
+    paths: compilerOptions.options.paths,
+  };
 }
