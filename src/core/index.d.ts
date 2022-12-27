5f140b76c447e9be7d44affaad3a40a9f229696e
Omar Othman <omar.othman@live.com>
2022-12-27 19:48:26 +0300

    Rename package report file

--- src/core/index.d.ts
+++ src/core/index.d.ts
@@ -3,6 +3,6 @@ export * from './analyzer';
 export * from './loader';
 export * from './module';
 export * from './modules';
-export * from './report';
+export * from './packageReport';
 export * from './report-compiler';
 export * from './reporter';
